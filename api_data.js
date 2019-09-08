28bb84f9778f6b9427e243fc6e91a9c2e942315f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-08 23:07:11 +0000

    Deploy sl-shinto-shrine-register/api to github.com/sl-shinto-shrine-register/api.git:gh-pages

---
+++ api_data.js
@@ -0,0 +1,11 @@
+define({ "api": [
+  {
+    "version": "0.1.0",
+    "type": "",
+    "url": "",
+    "filename": "routes/web.php",
+    "group": "_home_travis_build_sl_shinto_shrine_register_api_routes_web_php",
+    "groupTitle": "_home_travis_build_sl_shinto_shrine_register_api_routes_web_php",
+    "name": ""
+  }
+] });
---
+++ api_project.js
@@ -0,0 +1,15 @@
+define({
+  "name": "slsr-api",
+  "version": "0.1.0",
+  "description": "API of the Second Life Shinto shrine register (SLSR)",
+  "url": "https://api.slsr.org",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-09-08T23:07:05.379Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
