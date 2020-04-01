a5166bee0a2ed772a0d111264aab2b34ddbe0b3b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-01 11:57:26 +0000

    Deploy sl-shinto-shrine-register/api to github.com/sl-shinto-shrine-register/api.git:gh-pages

--- api_project.js
+++ api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-03-21T01:29:24.649Z",
+    "time": "2020-04-01T11:57:20.743Z",
     "url": "http://apidocjs.com",
     "version": "0.20.1"
   }
